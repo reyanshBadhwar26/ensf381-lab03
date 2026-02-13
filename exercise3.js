8474479d98cbbea9ab862eaf9d42a82929f7b64d
Gooogllleee <129225137+reyanshBadhwar26@users.noreply.github.com>
2026-02-13 12:13:39 -0700

    Exercise 2 and Exercise 3A done

---
+++ exercise3.js
@@ -0,0 +1,13 @@
+let classRoster = ["Alice", "Tom", "Charlie", "Diana","Evan"]
+
+console.log("Using ToString: " + classRoster.toString())
+console.log("classRoster: " + classRoster)
+
+classRoster.push("Fiona")
+classRoster.push("Nancy")
+
+console.log("classRoster.shift(): " + classRoster.shift())
+
+console.log("classRoster (after all changes): " + classRoster)
+
+console.log("classRoster length: " + classRoster.length)
